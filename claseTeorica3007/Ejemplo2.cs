b892075a6fdce7cd74322ff5e7b8bac21c34a446
Gisela Espinoza <ggarcia@ugb.edu.sv>
2021-07-30 11:58:30 -0600

    Se agregan dos ejemplos

---
+++ claseTeorica3007/Ejemplo2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace claseTeorica3007
+{
+    class Ejemplo2
+    {
+        static void Main(string[] args)
+        {
+            //instruccion switch
+
+            int a = 10;
+
+            /* switch (a)
+             {
+                 case 5:
+                     Console.WriteLine("El valor de a es 5");
+                     break;
+                 case 10:
+                     Console.WriteLine("El valor de a es 10");
+                     break;
+                 default:
+                     Console.WriteLine("El valor no es conocido");
+                     break;
+             }*/
+
+            switch (a)
+            {
+                case 10:
+                    Console.WriteLine("El valor de a es 10");
+                    switch (a - 1) //9
+                    {
+                        case 9:
+                            Console.WriteLine("El valor de a es 9");
+                            switch (a - 2)//que valor va a imprimir?
+                            {
+                                case 7:
+                                    Console.WriteLine("El valor de a es 7");
+                                    break;
+                                case 8:
+                                    Console.WriteLine("El valor de a es 8");
+                                    break;
+                                case 9:
+                                    Console.WriteLine("El valor de a es 9");
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+                case 5:
+                    Console.WriteLine("El valor de a es 5");
+                    break;
+                default:
+                    Console.WriteLine("El valor no es conocido");
+                    break;
+            }
+
+
+            Console.ReadKey();
+        }
+     }
+}
---
+++ claseTeorica3007/Ejemplo3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace claseTeorica3007
+{
+    class Ejemplo3
+    {
+
+        //ENUM
+        //es una clase especial que representa un grupo de constantes(variables inmutables, solo de lectura)
+        public enum Dias{ 
+            LUNES, MARTES, MIERCOLES, JUEVES, VIERNES
+        }
+        static void Main(string[] args) //metodo principal
+        {
+            //pedir datos al usuario
+            Console.WriteLine("Escriba el número correspondiente al día de la semana: ");
+            int dia = Int32.Parse(Console.ReadLine()); //conversion de tipo de dato
+            //int dia = Convert.ToInt32(Console.ReadLine()); //conversion de tipo de dato
+
+            switch (dia)
+            {
+                case 1:
+                    Console.WriteLine(Dias.LUNES);
+                    break;
+                case 2:
+                    Console.WriteLine(Dias.MARTES);
+                    break;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
